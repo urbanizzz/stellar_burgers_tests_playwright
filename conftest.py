f1290d3eb2d85a774e38a14048e19621c88fa11b
urbanizzz <yuraoskin@gmail.com>
2025-12-27 21:22:08 +0700

    fix: refactor authorized_user considering BasePage

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 from playwright.sync_api import Page
 
 from api.endpoints.authorization import Authorization
+from ui.base.base_page import BasePage
 from ui.data.test_data import AuthData
 from ui.data.urls import Urls
 
@@ -11,10 +12,9 @@ from ui.data.urls import Urls
 def authorized_user(page: Page):
     obj = Authorization(AuthData.email, AuthData.password)
     obj.check_response_status_code_200('Не прошла авторизация по API')
-    page.goto(Urls.base)
-    page.evaluate(f'localStorage.setItem("accessToken", '
-                  f'"{obj.get_access_token()}");')
-    page.evaluate(f'localStorage.setItem("refreshToken", '
-                  f'"{obj.get_refresh_token()}");')
+    page = BasePage(page, Urls.base)
+    page.open(Urls.base)
+    page.set_item('accessToken', obj.get_access_token())
+    page.set_item('refreshToken', obj.get_refresh_token())
     yield page
-    pass
+    page.close()
