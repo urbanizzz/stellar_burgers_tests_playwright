fd6f52a75ab95fdefd3c02f15decdc4da1b5df2e
urbanizzz <yuraoskin@gmail.com>
2026-01-05 02:05:14 +0700

    feat: test_burger_build_and_login

--- ui/tests/test_burger_build.py
+++ ui/tests/test_burger_build.py
@@ -3,7 +3,7 @@ import time
 import allure
 import pytest
 
-from ui.data.test_data import TestData
+from ui.data.test_data import TestData, AuthData
 from ui.pages.main_page import MainPage
 
 
@@ -17,5 +17,16 @@ def test_burger_build(authorized_user):
     page.set_item('refreshToken', authorized_user['refreshToken'])
 
     page.build_burger(TestData.burger)
+    actual_burger = page.get_basket_list()
+    page.assertions.assert_equal(actual_burger, TestData.burger)
+
+@allure.title('Авторизация после создания бургера')
+@allure.description("Бургер не должен измениться после авторизации")
+@pytest.mark.burger_build_and_login
+def test_burger_build_and_login(not_authorized_user):
+    page = MainPage(not_authorized_user['page'])
+    page.open_main_page()
+    page.build_burger(TestData.burger)
+    page.login(AuthData.email, AuthData.password)
     actual_burger = page.get_basket_list()
     page.assertions.assert_equal(actual_burger, TestData.burger)
