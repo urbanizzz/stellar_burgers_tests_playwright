0c42cf606f51d624aadc197a9d9f8abacd005cee
urbanizzz <yuraoskin@gmail.com>
2026-01-05 01:58:03 +0700

    feat: add basket_list_expect

--- ui/data/locators.py
+++ ui/data/locators.py
@@ -2,6 +2,7 @@
 class MainPageLocators:
     account_btn = '//a[@href="/account"]'
     drop_area = '//div[contains(@class, "constructor-element_pos_top")]'
+    basket_list_expect = 'div.constructor-element_pos_top'
     basket_list_element = '//span[@class="constructor-element__text"]'
 
     @staticmethod
